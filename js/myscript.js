b97c7a97732f169aaf2c257cb537e490506adc5d
Dmitry Ivanov <dimaivanov97@mail.ru>
2019-07-19 17:36:16 +0300

    first page

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+ $(document).ready(function(){
+            $(".navbar-toggler").click(function(){
+                $(".menu").toggleClass("displaynone")
+            });
+              $(".subscribe-button").click(function(){
+                 $(".myform").toggleClass("displaynone")                          
+                                           
+                                           });    
+
+          });
