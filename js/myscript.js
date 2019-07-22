db656b72f7ad27ec31acea53bc8c8873c3a0be23
Dmitry Ivanov <dimaivanov97@mail.ru>
2019-07-22 18:05:39 +0300

    first page nearly finished

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,11 @@
  $(document).ready(function(){
             $(".navbar-toggler").click(function(){
-                $(".menu").toggleClass("displaynone")
+                $(".menu").toggleClass("displaynone");
             });
               $(".subscribe-button").click(function(){
-                 $(".myform").toggleClass("displaynone")                          
+                 $(".myform").toggleClass("displaynone");                          
                                            
-                                           });    
-
+                                           });
+     
+            $(".carousel").attr(data-pause, false);
           });
